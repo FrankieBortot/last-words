945dd37ae7901f1c3dc3f7e32daaf4c211173292
Frankie Bortot <bortot.frankie@gmail.com>
2020-09-23 22:51:01 +0200

    reveal + scroll
    aggiunto script per mostrare div al click e smooth-scroll automatico

---
+++ script.js
@@ -0,0 +1,23 @@
+function reveal2() {
+    document.getElementById("part2").style.display = "block";
+}
+
+function reveal3() {
+    document.getElementById("part3").style.display = "block";
+}
+
+function reveal4() {
+    document.getElementById("part4").style.display = "block";
+}
+
+function reveal5() {
+    document.getElementById("part5").style.display = "block";
+}
+
+function reveal6() {
+    document.getElementById("part6").style.display = "block";
+}
+
+function reveal7() {
+    document.getElementById("part7").style.display = "block";
+}
