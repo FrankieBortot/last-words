c495001c5d80958d55dae45d78a65e726f1418dc
Frankie Bortot <bortot.frankie@gmail.com>
2020-09-27 12:43:28 +0200

    fix

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function reveal(elementid) {
+
     document.getElementById(elementid).style.display = "block";
+
 }
 
+function changetheme() {
+
+    var x = document.getElementById("background");
+
+  if (x.className === "light") {
+
+    x.className = "dark";
+
+  } else {
+
+    x.className = "light";
+
+  }
+
+}
 
 /*
 
