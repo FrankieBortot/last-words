948c6839e024510fc983d39ff8f5cd3acd606f51
Frankie Bortot <bortot.frankie@gmail.com>
2020-11-10 21:37:35 +0100

    fix

--- script.js
+++ script.js
@@ -10,12 +10,31 @@ function SubForm() {
     type:'post',
     data:$("#myForm").serializeArray(),
     success: function(){
-      document.getElementById("success").style.display = "block"
+      document.getElementById("success").style.display = "block",
+      window.location.href = "credits.html"
     },
     error: function(){
       document.getElementById("error").style.display = "block"
     }
   });
+}
+
+$(document).ready(function() {
+  $('#inputWrapper').on('keyup', '#message', function() {
+    if($(this).val() == '') {
+      $('div#final-button').text('Non ho niente da dire');
+      $('div#final-button').removeClass('special');
+      $('div#final-button').addClass('continue');
+    } else {
+      $('div#final-button').text('Invii il messaggio');
+      $('div#final-button').removeClass('continue');
+      $('div#final-button').addClass('special');
+    }
+  });
+});
+
+function waiting() {
+  document.getElementById('final-button').innerHTML = 'invio in corso...';
 }
 
 
